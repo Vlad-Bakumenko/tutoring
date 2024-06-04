7a3658b1a4f215682b71390f6727c174651c4eed
Vladyslav Bakumenko <143517551+Vlad-Bakumenko@users.noreply.github.com>
2024-06-04 21:19:43 +0200

    Delete 2024 - 06 - 04 directory

--- 2024 - 06 - 04/middleware/checkRole.js
+++
@@ -1,9 +0,0 @@
-export const checkRole = (req,res,next) => {
-    if (!req.user) {
-        return res.status(401).json({message: "No authenticated user"})
-    }
-    if (req.user.role !== "admin") {
-        return res.status(403).json({message: "Only admin can perform this operation"})
-    }
-    next();
-}
--- 2024 - 06 - 04/middleware/requestlogger.js
+++
@@ -1,4 +0,0 @@
-export default function requestLogger(req, res, next) {
-  console.log(`Request: ${req.method} ${req.url}`);
-  next();
-}
